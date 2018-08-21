21d3c6ba30e85e8d1c1cfa0663531769a21741b8
Nick McCurdy <nick@nickmccurdy.com>
2018-08-21 19:51:52 -0400

    Format declarations

--- types/selectorator/index.d.ts
+++ types/selectorator/index.d.ts
@@ -8,18 +8,24 @@ import { OutputSelector, Selector } from "reselect";
 
 export as namespace selectorator;
 
-export type Path<S, R> = Selector<S, R> | string | number | Array<string | number>;
-export type ArgumentPath<S, R> = Path<S, R> | { path: Path<S, R>; argIndex: number };
+export type Path<S, R> =
+  | Selector<S, R>
+  | string
+  | number
+  | Array<string | number>;
+export type ArgumentPath<S, R> =
+  | Path<S, R>
+  | { path: Path<S, R>; argIndex: number };
 
 export interface Options<O extends any[] = any[]> {
-    deepEqual: boolean;
-    isEqual: typeof deepEqual;
-    memoizer: <F extends (...options: O) => any>(func: F, ...options: O) => F;
-    memoizerParams: O;
+  deepEqual: boolean;
+  isEqual: typeof deepEqual;
+  memoizer: <F extends (...options: O) => any>(func: F, ...options: O) => F;
+  memoizerParams: O;
 }
 
 export default function createSelector<S, R extends any[], T>(
-    paths: [...Array<ArgumentPath<S, R>>] | Record<string, ArgumentPath<S, R>>,
-    getComputedValue?: (...results: R) => T,
-    options?: Partial<Options>
+  paths: [...Array<ArgumentPath<S, R>>] | Record<string, ArgumentPath<S, R>>,
+  getComputedValue?: (...results: R) => T,
+  options?: Partial<Options>
 ): OutputSelector<S, T, (...results: R) => T>;
